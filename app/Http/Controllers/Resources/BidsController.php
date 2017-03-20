d600ae6ce9c3944f97b030946c2c923d6eeadfbf
Yeap Hooi Tong <hooitongx@gmail.com>
2017-03-20 17:44:11 +0800

    :hammer: Updated `bids` resource controller

--- app/Http/Controllers/Resources/BidsController.php
+++ app/Http/Controllers/Resources/BidsController.php
@@ -2,6 +2,7 @@
 
 namespace Taskr\Http\Controllers\Resources;
 
+use Illuminate\Http\Request;
 use Taskr\Bid;
 use Taskr\Http\Controllers\Controller;
 use Taskr\Task;
@@ -21,24 +22,28 @@ class BidsController extends Controller
     | These methods should return views with the appropriate data bind to it.
     |
     */
-    public function index()
-    {
 
-    }
-
+    /**
+     * Display a listing of the bid such as the bid details (date, cost, etc).
+     *
+     * @param \Taskr\Bid $bid
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(Bid $bid)
     {
-
+        return view("bids.show");
     }
 
+    /**
+     * Display a form to create a new bid. If this is not needed in finalized design decision, restrict this
+     * method to administrators only.
+     *
+     ** @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create()
     {
-
-    }
-
-    public function edit()
-    {
-
+        return view("bids.create");
     }
 
     /*
@@ -50,16 +55,55 @@ class BidsController extends Controller
     | appropriate actions instead.
     |
     */
-    public function update()
-    {
 
+    /**
+     * Return all the bids from a task. Accept some parameters to filter the bids.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Taskr\Task              $task
+     */
+    public function index(Request $request, Task $task)
+    {
+        # if request query has user, add user id into the filter of the request
+        if ($request->has('user')) {
+            $user_id = $request->input('user')->id;
+            return DB::select('select * from bids where task_id = ? and user_id = ?', [$task->id, $user_id]);
+        } else {
+            return DB::select('select * from bids where task_id = ? and user_id = ?', [$task->id]);
+        }
     }
 
-    public function delete()
-    {
 
+    /**
+     * Update the selected field of the bid, whether is it true or false.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Taskr\Bid               $bid
+     *
+     * @internal param $id
+     */
+    public function update(Request $request, Bid $bid)
+    {
+        if ($request->has('selected')) {
+            return DB::update('update bids set selected = ? where id = ?', [$request->input('selected'), $bid->id]);
+        }
     }
 
+    /**
+     * This method must be restricted to administrator access as user should not be allowed to delete a bid.
+     *
+     * @param \Taskr\Bid $bid
+     */
+    public function delete(Bid $bid)
+    {
+        return DB::delete('delete from bids where id = ?', [$bid->id]);
+    }
+
+    /**
+     * @param \Taskr\Task $task
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(Task $task)
     {
         $task->addBid(request('price'));
